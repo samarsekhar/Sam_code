68623ba2c26207da975eff64a30a39cd40bd956b
samar <samarsekhar25@gmail.com>
2022-01-07 15:46:49 +0530

    commit

---
+++ sam/JS-Part1/Operators.one.js
@@ -0,0 +1,18 @@
+var a = 10;
+var b = 20;
+
+var c = a + b
+var d = a * b
+
+var e = a + 'sam'
+var f = a + '500'
+var g = a * 'sam'
+var h = a * '200'
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(f);
+console.log(g);
+console.log(h);
